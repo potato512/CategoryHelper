f7093c03e59d89463f91bd0dda3f170b36898d8f
zhangshaoyu <snow@zhangshaoyudeiMac.local>
2019-08-31 17:12:41 +0800

    添加标签点击示例

---
+++ tmp/DemoLabelClick/DemoLabelClick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoLabelClick
+//
+//  Created by zhangshaoyu on 2019/8/1.
+//  Copyright © 2019 zhangshaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tmp/DemoLabelClick/DemoLabelClick/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DemoLabelClick
+//
+//  Created by zhangshaoyu on 2019/8/1.
+//  Copyright © 2019 zhangshaoyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
