61c06931dd3d5e74833e3823bda93e2c114ea6f2
devZhang <zhangsy757@163.com>
2020-07-18 19:27:53 +0800

    status修改

--- SYCategory/SYCategoryUIKit/UIView+Status.h
+++ SYCategory/SYCategoryUIKit/UIView+Status.h
@@ -8,79 +8,54 @@
 
 #import <UIKit/UIKit.h>
 
-/// 对齐方式（顶端对齐、居中对齐-默认、底端对齐）
-typedef NS_ENUM(NSInteger, StatusViewAlignment) {
-    /// 对齐方式-居中对齐-默认
-    StatusViewAlignmentDefault = 0,
-    /// 对齐方式-顶端对齐
-    StatusViewAlignmentTop = 1,
-    /// 对齐方式-居中对齐-默认
-    StatusViewAlignmentCenter = StatusViewAlignmentDefault,
-    /// 对齐方式-底端对齐
-    StatusViewAlignmentBottom = 2
-};
-
 @interface UIView (Status)
 
-/// 对齐方式（顶端对齐、居中对齐-默认、底端对齐）
-@property (nonatomic, assign) StatusViewAlignment statusViewAlignment;
-/// 按钮全屏（默认：NO；全屏时，按钮不显示）
-@property (nonatomic, assign) BOOL statusButtonFullScreen;
-/// 多图时的动画时间（默认：0.6）
-@property (nonatomic, assign) NSTimeInterval statusAnimationTime;
+/// 添加状态视图方法及回调 UIActivityIndicatorView
+- (void)addStatusActivityViewWithConfigurationHandler:(void (^)(UIActivityIndicatorView *activityView))configurationHandler;
 
-@property (nonatomic, strong, readonly) UIView *statusView;
-@property (nonatomic, strong, readonly) UIActivityIndicatorView *statusActivityView;
-@property (nonatomic, strong, readonly) UIImageView *statusImageView;
-@property (nonatomic, strong, readonly) UILabel *statusMessageLabel;
-@property (nonatomic, strong, readonly) UIButton *statusButton;
+/// 添加状态视图方法及回调 UIImageView UILabel UIButton
+- (void)addStatusViewWithConfigurationHandler:(void (^)(UIImageView *imageView, UILabel *label, UIButton *button))configurationHandler click:(void (^)(void))clickHandle;
 
-/**
- *  开始（默认：菊花转）
- */
-- (void)statusViewLoadStart;
-/**
- *  开始（自定义：提示语、图标）
- *
- *  @param message 提示语
- *  @param images  图标数组
- */
-- (void)statusViewLoadStart:(NSString *)message image:(NSArray <UIImage *> *)images;
-
-/**
- *  结束，加载成功（默认：无提示语、无图标）
- */
-- (void)statusViewLoadSuccess;
-/**
- *  结束，加载成功，无数据（自定义：提示语、图标；无重新加载）
- *
- *  @param message 提示语
- *  @param images  图标数组
- */
-- (void)statusViewLoadSuccessWithoutData:(NSString *)message image:(NSArray <UIImage *> *)images;
-/**
- *  结束，加载成功，无数据（自定义：提示语、图标；重新加载）
- *
- *  @param message 提示语
- *  @param images  图标数组
- *  @param click   重新加载
- */
-- (void)statusViewLoadSuccessWithoutData:(NSString *)message image:(NSArray <UIImage *> *)images click:(void (^)(void))click;
-
-/**
- *  结束，加载失败（自定义：提示语、图标；无重新加载）
- *
- *  @param message 提示语
- *  @param images  图标数组
- */
-- (void)statusViewLoadFailue:(NSString *)message image:(NSArray <UIImage *> *)images;
-/**
- *  结束，加载失败（自定义：提示语、图标；重新加载）
- *
- *  @param message 提示语
- *  @param images  图标数组
- *  @param click   重新加载
- */
-- (void)statusViewLoadFailue:(NSString *)message image:(NSArray <UIImage *> *)images click:(void (^)(void))click;
+/// 移动状态视图
+- (void)removeStatusView;
 
 @end
+
+/**
+使用示例
+
+// 加载指示符
+__weak UIViewController *weakSelf = self;
+[self.view addStatusActivityViewWithConfigurationHandler:^(UIActivityIndicatorView *activityView) {
+    activityView.center = weakSelf.view.center;
+    activityView.color = kColorBlue;
+    [activityView startAnimating];
+}];
+
+// 移动
+[self.view removeStatusView];
+
+// 显示状态子视图
+__weak UIViewController *weakSelf = self;
+[self.view addStatusViewWithConfigurationHandler:^(UIImageView *imageView, UILabel *label, UIButton *button) {
+    // imageView属性设置
+    imageView.frame = CGRectMake((weakSelf.view.frame.size.width - 150) / 2, (weakSelf.view.frame.size.height - 150 - 30) / 2, 150, 150);
+    imageView.image = [UIImage imageNamed:@"loading_no_data"];
+
+    // label属性设置
+    label.frame = CGRectMake(20, (imageView.frame.origin.y + imageView.frame.size.height), weakSelf.view.frame.size.width, 30);
+    label.textColor = UIColor.blackColor;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontSize:14];
+    label.text = @"请求失败";
+
+    // button属性设置
+    button.frame = CGRectMake(20, label.bottom, (label.frame.origin.y + label.frame.size.height), 30);
+    [button setTitle:@"重新加载" forState:UIControlStateNormal];
+    [button setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+} click:^{
+    [weakSelf.view removeStatusView];
+    [weakSelf loadData];
+}];
+
+*/
